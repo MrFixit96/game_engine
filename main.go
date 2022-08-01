f002fd1ef662ddcf1e514b3fd361be655fca268f
MrFixit96 <james@janderton.com>
2022-08-01 17:26:43 -0500

    fixed cmd/server import

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main // import "github.com/mrfixit96/game_engine
 import (
 	"os"
 
-	"github.com/mrfixit96/game_engine/cmd"
+	"github.com/mrfixit96/game_engine/cmd/server"
 )
 
 func main() {
